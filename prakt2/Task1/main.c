63f668f9c05cec8e8529519f774224ad7e6a22b3
Axelessss <160502890+axelessss@users.noreply.github.com>
2024-03-21 20:28:46 +0700

    asdfasd
    asfdsf

--- prakt2/Task1/main.c
+++ prakt2/Task1/main.c
@@ -22,13 +22,13 @@ typedef struct Job
 typedef struct Phone
 {
     char phone_type[M];
-    char phone[M];
+    char number[M];
 }Phone;
 
 typedef struct Email
 {
     char email[N];
-}Email
+}Email;
 
 typedef struct Social
 {
@@ -54,7 +54,7 @@ typedef struct Contact
 }Contact;
 
 
-void add(struct contact &contacts)
+void add(Contact &contacts)
 {
 
 }
@@ -63,55 +63,130 @@ void add()
 {
     char con;
     short operation;
-    new_contact = new contact();
-    new_contact.id = 0;
-    printf("Введите фамилию: ");
-    fgets(new_contact.surname, sizeof(new_contact.surname, stdin));
-    printf("Введите имя: ");
-    fgets(new_contact.name, sizeof(new_contact.name, stdin));
+    Contact contact;
+    contact.id = 0;
+    int count;
+    bool shutdown = false;
 
-    printf("Желаете ли продолжить заполнение? Д/н")
+    char buffer[N];
+    //char buffer2[N];
+
+    while (true)
+    {
+        printf("Введите фамилию: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        if (strlen(buffer)==0)
+            printf("Поле не должно быть пустым!");
+        else
+        {
+            strncpy(contact.fio.surname, buffer, N);
+            break;
+        }
+    }
+
+    while (true)
+    {
+        printf("Введите имя: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        if (strlen(buffer)==0)
+            printf("Поле не должно быть пустым!");
+        else
+        {
+            strncpy(contact.fio.name, buffer, N);
+            break;
+        }
+    }
+    
+
+    printf("Желаете ли продолжить заполнение? Д/н");
     scanf("%c", &con);
     if (con == 'Д'||con == 'д')
         while (true)
         {       
-            printf("Что заполнить:\n1-Отчество\n2-Место работы\n3-Должность\n4-Номер телефона\n5-E-mail\n6-Соцсети\n7-Мессенджеры");
+            if (shutdown)
+                break;
+
+            printf("Что заполнить:\n1-Отчество\n2-Место работы\n3-Должность\n4-Номер телефона\n5-E-mail\n6-Соцсети\n7-Мессенджеры\nЛюбая другая кнопка - Выход");
             scanf("%d", &operation);
             switch(operation)
             {
                 case 1:
                     printf("Введите отчество");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                    fgets(contact.fio.partonymic, sizeof(contact.fio.partonymic), stdin);
                     break;
                 
                 case 2:
                     printf("Введите  место работы");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                    fgets(contact.job.corp, sizeof(contact.job.corp), stdin);
+
+                    printf("Введите  должность");
+                    fgets(contact.job.title, sizeof(contact.job.title), stdin);
                     break;
                 
-                case 2:
-                    printf("Введите должность");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                case 3:
+                    printf ("Введите количество номеров телефона: ");
+                    scanf("%d", &count);
+                    for(int i=0;i<count;i++)
+                    {
+                        printf("Укажите тип телефона: ");      
+                        fgets(contact.phone[i].phone_type, sizeof(contact.phone[i].phone_type), stdin);
+
+                        printf("Укажите номер телефона: ");      
+                        fgets(contact.phone[i].number, sizeof(contact.phone[i].number), stdin);
+                    }
                     break;
 
-                case 2:
-                    printf("Введите отчество");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                case 4:
+                    printf ("Введите количество номеров телефона: ");
+                    scanf("%d", &count);
+                    for(int i=0;i<count;i++)
+                    {
+                        printf("Укажите тип телефона: ");      
+                        fgets(contact.phone[i].phone_type, sizeof(contact.phone[i].phone_type), stdin);
+
+                        printf("Укажите номер телефона: ");      
+                        fgets(contact.phone[i].number, sizeof(contact.phone[i].number), stdin);
+                    }
                     break;
 
-                case 2:
-                    printf("Введите отчество");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                case 5:
+                    printf ("Введите количество e-mail адресов: ");
+                    scanf("%d", &count);
+                    for(int i=0;i<count;i++)
+                    {
+                        printf("Введите электронный адрес: ");      
+                        fgets(contact.phone[i].number, sizeof(contact.phone[i].number), stdin);
+                    }
                     break;
 
-                case 2:
-                    printf("Введите отчество");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                case 6:
+                    printf ("Введите количество профилей в соцсетях: ");
+                    scanf("%d", &count);
+                    for(int i=0;i<count;i++)
+                    {
+                        printf("Укажите название соцсети: ");      
+                        fgets(contact.phone[i].phone_type, sizeof(contact.phone[i].phone_type), stdin);
+
+                        printf("Укажите ссылку на ваш профиль: ");      
+                        fgets(contact.phone[i].number, sizeof(contact.phone[i].number), stdin);
+                    }
                     break;
 
-                case 2:
-                    printf("Введите отчество");
-                    fgets(new_contact.partonymic, sizeof(new_contact.partonymic), stdin);
+                case 7:
+                    printf ("Введите количество профилей в мессенджерах: ");
+                    scanf("%d", &count);
+                    for(int i=0;i<count;i++)
+                    {
+                        printf("Укажите название мессенджера: ");      
+                        fgets(contact.phone[i].phone_type, sizeof(contact.phone[i].phone_type), stdin);
+
+                        printf("Укажите имя пользователя: ");      
+                        fgets(contact.phone[i].number, sizeof(contact.phone[i].number), stdin);
+                    }
+                    break;
+                
+                default:
+                    shutdown = true;
                     break;
                 
             }
@@ -120,7 +195,7 @@ void add()
 
 int main()
 {
-    struct contact contacts[21];
+    Contact contacts[21];
 
 
 }
--- prakt2/Task2/main.c
+++ prakt2/Task2/main.c
@@ -2,25 +2,94 @@
 #include <math.h>
 #include <stdbool.h>
 
-void sum()
+bool input(int operation, bool shutdown)
 {
+    float a, b;
+    float result;
+    if (!shutdown);
+        printf("Введите первый аргумент: ");
+        scanf("%f", &a);
+
+    switch(operation)
+        {
+            case 1:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = sum(a, b);
+                break;
+            
+            case 2:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = subtract(a, b);
+                break;
+            
+            case 3:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = mul(a, b);
+                break;
+            
+            case 4:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = div(a, b);
+                break;
+            
+            case 5:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = deg(a, b);
+                break;
+            
+            case 6:
+                printf("Введите второй аргумент: ");
+                scanf("%f", &b);
+                result = root(a, b);
+                break;
+            
+            case 7:
+                
+                result = e(a);
+                break;
+            
+            case 8:
+                shutdown=true;
+                break;
+            
+            default:
+                printf("Неопознанная команда");
+                break;
+        }
+
+    
+
+    if (shutdown)
+        return true;
+    else
+    {
+        printf("Результат = %f\n", result);
+        return false;
+    }
+}
+
+void sum(float a, float b)
+{
+    return a+b;
+}
+
+void subtract(float a, float b)
+{
+    float a, b;
     printf("Введите первый аргумент: ");
     scanf("%f", &a);
     printf("Введите второй аргумент: ");
     scanf("%f", &b);
-    printf("Результат = %f\n", a+b);
 }
 
-void subtract()
+void mul(float a, float b)
 {
-    printf("Введите первый аргумент: ");
-    canf("%f", &a);
-    printf("Введите второй аргумент: ");
-    scanf("%f", &b);
-}
-
-void mul()
-{
+    float a, b;
     printf("Введите первый аргумент: ");
     scanf("%f", &a);
     printf("Введите второй аргумент: ");
@@ -28,8 +97,9 @@ void mul()
     printf("Результат = %f\n", a*b);
 }
 
-void div()
+void div(float a, float b)
 {
+    float a, b;
     printf("Введите первый аргумент: ");
     scanf("%f", &a);
     printf("Введите второй аргумент: ");
@@ -37,8 +107,9 @@ void div()
     printf("Результат = %f\n", a/b);
 }
 
-void deg()
+void deg(float a, float b)
 {
+    float a, b;
     printf("Введите основание степени: ");
     scanf("%f", &a);
     printf("Введите показатель степени: ");
@@ -46,8 +117,9 @@ void deg()
     printf("Результат = %f\n", pow(a, b));
 }
 
-void root()
+void root(float a, float b)
 {
+    float a, b;
     printf("Введите подкоренной аргумент: ");
     scanf("%f", &a);
     printf("Введите показатель корня: ");
@@ -55,17 +127,16 @@ void root()
     printf("Результат = %f\n", pow(a, 1/b));
 }
 
-void e()
+void e(float a, float b)
 {
+    float a, b;
     printf("Введите аргумент: ");
-    canf("%f", &a);
+    scanf("%f", &a);
     printf("Результат = %f\n", exp(a));
 }
 
 int main()
 {
-    float a;
-    float b;
     bool shutdown=false;
     int operation;
 
@@ -77,47 +148,6 @@ int main()
         printf("\nВыберите действие: ");
         printf("\n1-сложение\n2-вычитание\n3-умножение\n4-деление\n5-возведение в степень\n6-корень n-й степени\n7-экспонента\n8-завершить работу\n");
         scanf("%d", &operation);
-
-        switch(operation)
-        {
-            case 1:
-                sum();
-                break;
-            
-            case 2:
-                subtract();
-                break;
-            
-            case 3:
-                mul();
-                break;
-            
-            case 4:
-                
-                div();
-                break;
-            
-            case 5:
-                
-                deg();
-                break;
-            
-            case 6:
-                root();
-                break;
-            
-            case 7:
-                
-                e();
-                break;
-            
-            case 8:
-                shutdown=true;
-                break;
-            
-            default:
-                printf("Неопознанная команда");
-                break;
-        }
+        shutdown = input(operation, shutdown);
     }
 }
