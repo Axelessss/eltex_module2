436238cafa54d2735707f0c54eff18663047409e
Axelessss <kicib4212@yandex.ru>
2024-03-28 11:01:26 +0700

    prakt 3 task 2

--- prakt3/Task2/main.c
+++ prakt3/Task2/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 10
+
+void generate(int (*Dst)[N], int count)
+{
+
+}
+
+void get_nums(char *addr)
+{
+
+}
 
 int main(int argc, char* argv[])
 {
+    int IP_Srcp[N];
+    char *src[N];
+    char *masks[N];
+    int *IP_Dst[N];
+    int mask[N];
+    int count;
+
+    src = split(argv[1], ".");
+
+
     
 }
